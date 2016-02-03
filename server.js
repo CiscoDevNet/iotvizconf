f34ab22cea89fba223621f8769695b458abd4ec8
Varun Seereeram <vseereer@cisco.com>
2016-02-02 18:40:44 -0800

    removed unnecessary files

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
-app.use('/api', proxy('http://localhost', {
+app.use('/api', proxy('http://ctao-sdn-03', {
     port: 8181,
     forwardPath: function(req, res) {
         return url.parse(req.url).path;
